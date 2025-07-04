95d7611e294401c7ede005a30c17c6b847928fd2
parasdshah <parasdshah@users.noreply.github.com>
2025-07-05 01:57:57 +0530

    Fix Home Page Layout

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -174,17 +174,15 @@ const Home: React.FC = () => {
             onClick={() => navigate('/recipes')}
           />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-            {featuredRecipes.map((recipe) => (
-              <RecipeCard
-                key={recipe.id}
-                recipe={recipe}
-                onSave={handleSaveRecipe}
-                isSaved={savedRecipes.has(recipe.id)}
-              />
-            ))}
-          </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {featuredRecipes.map((recipe) => (
+            <RecipeCard
+              key={recipe.id}
+              recipe={recipe}
+              onSave={handleSaveRecipe}
+              isSaved={savedRecipes.has(recipe.id)}
+            />
+          ))}
         </div>
       </section>
 
