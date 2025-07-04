18a132a262198015987c938df35cee7c769ec96b
parasdshah <parasdshah@users.noreply.github.com>
2025-07-05 01:56:40 +0530

    Update Home Page to 4-Column Recipe Layout

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -175,6 +175,7 @@ const Home: React.FC = () => {
           />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {featuredRecipes.map((recipe) => (
             <RecipeCard
               key={recipe.id}
