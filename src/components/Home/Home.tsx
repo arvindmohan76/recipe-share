4c526bcbfc8d98d869d9ea7190dbb306fa3c2c30
parasdshah <parasdshah@users.noreply.github.com>
2025-07-05 01:57:14 +0530

    Updated Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -175,15 +175,16 @@ const Home: React.FC = () => {
           />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          {featuredRecipes.map((recipe) => (
-            <RecipeCard
-              key={recipe.id}
-              recipe={recipe}
-              onSave={handleSaveRecipe}
-              isSaved={savedRecipes.has(recipe.id)}
-            />
-          ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {featuredRecipes.map((recipe) => (
+              <RecipeCard
+                key={recipe.id}
+                recipe={recipe}
+                onSave={handleSaveRecipe}
+                isSaved={savedRecipes.has(recipe.id)}
+              />
+            ))}
+          </div>
         </div>
       </section>
 
